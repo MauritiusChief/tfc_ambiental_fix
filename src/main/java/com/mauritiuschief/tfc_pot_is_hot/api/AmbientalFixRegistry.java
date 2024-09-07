51fdf6a332ca7f411c68185bb80dd3c1546cf220
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2024-09-07 10:43:33 -0400

    尝试修复好像没有register成功

--- src/main/java/com/mauritiuschief/tfc_pot_is_hot/api/AmbientalFixRegistry.java
+++ src/main/java/com/mauritiuschief/tfc_pot_is_hot/api/AmbientalFixRegistry.java
@@ -1,12 +1,34 @@
 package com.mauritiuschief.tfc_pot_is_hot.api;
 
-import com.lumintorious.tfcambiental.api.AmbientalRegistry;
+// import com.lumintorious.tfcambiental.api.AmbientalRegistry;
 // import com.lumintorious.tfcambiental.api.BlockEntityTemperatureProvider;
+import com.lumintorious.tfcambiental.api.BlockEntityTemperatureProvider;
 
-public class AmbientalFixRegistry {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Optional;
+
+public class AmbientalFixRegistry<Type> implements Iterable<Type> {
+
+    public static final AmbientalFixRegistry<BlockEntityTemperatureProvider> BLOCK_ENTITIES = new AmbientalFixRegistry<>();
+
     static {
         // Register your methods with the existing registry
-        AmbientalRegistry.BLOCK_ENTITIES.register(EnhancedBlockEntityTemperatureProvider::handlePot);
-        AmbientalRegistry.BLOCK_ENTITIES.register(EnhancedBlockEntityTemperatureProvider::handleGrill);
+        BLOCK_ENTITIES.register(EnhancedBlockEntityTemperatureProvider::handlePot);
+        BLOCK_ENTITIES.register(EnhancedBlockEntityTemperatureProvider::handleGrill);
+    }
+
+    private final ArrayList<Type> list = new ArrayList<>();
+
+    private AmbientalFixRegistry() {
+    }
+
+    public void register(Type type) {
+        list.add(type);
+    }
+
+    @Override
+    public Iterator<Type> iterator() {
+        return list.iterator();
     }
 }
